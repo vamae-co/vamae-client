15a8e56143278add829caf25bbe032c2c3a8868e
MihaVar <mykhailvarych@gmail.com>
2024-03-25 17:51:18 +0200

    Inited Connect4Socket

---
+++ src/app/components/Connect4Socket.jsx
@@ -0,0 +1,13 @@
+import {Stomp} from "@stomp/stompjs";
+import * as SockJS from 'sockjs-client';
+
+//let stompClient = null;
+
+// function connect() {
+//     const socket = new SockJS('/ws');
+//     stompClient = Stomp.over(socket);
+//
+//     let token = localStorage.getItem('token');
+//     stompClient.connect({token}, onConnected, onError);
+// }
+
